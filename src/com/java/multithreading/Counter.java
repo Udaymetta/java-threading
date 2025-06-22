ace6cdaae16c9adac85a6ce9ba70548305726d80
Udaymetta <mudayk99@gmail.com>
2025-06-22 23:55:59 +0530

    MultiThread changes

--- src/com/java/multithreading/Counter.java
+++ src/com/java/multithreading/Counter.java
@@ -3,8 +3,23 @@ package com.java.multithreading;
 public class Counter {
     int count;
 
-    public synchronized void increment(){
-        System.out.println(Thread.currentThread().getName());
-        count++;
+    public synchronized void syncIncrement(){
+        try {
+            Thread.sleep(100);
+            System.out.println(Thread.currentThread().getName());
+            count++;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void increment(){
+        try {
+            Thread.sleep(100);
+            System.out.println(Thread.currentThread().getName());
+            count++;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/com/java/multithreading/MultiThread.java
+++ src/com/java/multithreading/MultiThread.java
@@ -3,6 +3,11 @@ package com.java.multithreading;
 public class MultiThread {
 
     public static void main(String[] args) throws InterruptedException {
+        raceCounter();
+        synchronizedCounter();
+    }
+
+    private static void raceCounter() throws InterruptedException {
         Counter counter = new Counter();
         Thread thread1 = new Thread(() -> {
             for (int i = 1 ; i<= 50 ; i++){
@@ -12,6 +17,25 @@ public class MultiThread {
         Thread thread2 = new Thread(() -> {
             for (int i = 1 ; i<= 50 ; i++){
                 counter.increment();
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();// wait for thread1 to finish
+        thread2.join();// wait for thread2 to finish
+        System.out.println(counter.count);
+    }
+
+    private static void synchronizedCounter() throws InterruptedException {
+        Counter counter = new Counter();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 1 ; i<= 50 ; i++){
+                counter.syncIncrement();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 1 ; i<= 50 ; i++){
+                counter.syncIncrement();
             }
         });
         thread1.start();
